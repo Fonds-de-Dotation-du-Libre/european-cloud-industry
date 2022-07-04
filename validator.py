ea67c3404cee027cc166b71cca5bbf2aca8bab92
EyaChafroud2 <102510753+EyaChafroud2@users.noreply.github.com>
2022-07-04 15:05:31 +0200

    Update validator.py

--- validator.py
+++ validator.py
@@ -1,1 +1,39 @@
 
+here’s a script to run the checks:
+
+```
+#!/usr/bin/env python3
+
+import glob, json, os
+
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
+
+SCHEMA = json.load(open("data/JSON-schema.json"))
+
+num_valid = 0
+
+filenames = sorted(glob.glob("data/*.json"))
+for fn in filenames:
+    if fn == "data/JSON-schema.json":
+        continue
+
+    try:
+        data = json.load(open(fn))
+    except:
+        print(f"{fn} -> syntactically incorrect JSON")
+        continue
+
+    try:
+        validate(instance=data, schema=SCHEMA[0])
+    except ValidationError as e:
+        print(f"{fn} -> invalid JSON")
+        print("   " + e.message)
+        continue
+
+    print(f"{fn} -> OK")
+    num_valid += 1
+
+
+print(f"{num_valid} valid JSON files")
+```
