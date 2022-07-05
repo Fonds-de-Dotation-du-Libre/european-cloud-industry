accd8363c0c0573e0d4c80a8ce97904d1bb6811e
slaimu <xiaowu.zhangv@gmail.com>
2022-07-05 11:08:11 +0200

    fix json shema validator

--- validator.py
+++ validator.py
@@ -1,11 +1,6 @@
-
-here’s a script to run the checks:
-
-```
 #!/usr/bin/env python3
 
-import glob, json, os
-
+import glob, json, os, sys
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError
 
@@ -13,27 +8,36 @@ SCHEMA = json.load(open("data/JSON-schema.json"))
 
 num_valid = 0
 
-filenames = sorted(glob.glob("data/*.json"))
-for fn in filenames:
-    if fn == "data/JSON-schema.json":
-        continue
+if len(sys.argv) > 1:
+  filenames = sys.argv[1:]
+else:
+  filenames = sorted(glob.glob("*.json"))
+incorrect_json_list = []
+invalid_json_list = []
 
+for fn in filenames:
     try:
         data = json.load(open(fn))
     except:
-        print(f"{fn} -> syntactically incorrect JSON")
+        incorrect_json_list.append(fn)
         continue
 
     try:
         validate(instance=data, schema=SCHEMA[0])
     except ValidationError as e:
-        print(f"{fn} -> invalid JSON")
-        print("   " + e.message)
+        invalid_json_list.append({"file":fn, "error": e.message})
         continue
 
     print(f"{fn} -> OK")
     num_valid += 1
 
 
-print(f"{num_valid} valid JSON files")
-```
+print(f"{num_valid} valid JSON files\n")
+print("\nfollowing file are incorrect:\n")
+for fn in incorrect_json_list:
+   print(f"{fn}")
+
+print("\nfollowing file are invalide:\n")
+for fn in invalid_json_list:
+  print(f"{fn['file']}: {fn['error']}")
+
